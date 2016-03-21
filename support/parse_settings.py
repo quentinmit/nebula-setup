610657ded44f6ae1fcde3a4181c2338007d1678b
Martastain <martas@imm.cz>
2016-03-21 16:22:37 +0100

    etc config parser

--- support/parse_settings.py
+++ support/parse_settings.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python
+import os
 import sys
 import json
 
-with open("local_settings.json") as f:
-    config = json.load(f)
+settings_files = [
+    "/etc/nebula.json",
+    "local_settings.json"
+]
 
-print config[sys.argv[1]]
+config = {}
+for settings_file in settings_files:
+    if os.path.exists(settings_file):
+        try:
+            config.update(json.load(open(settings_file)))
+        except:
+            pass
+
+try:
+    print config[sys.argv[1]]
+except:
+    sys.exit(1)
