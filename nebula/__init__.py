efb3b30ff683223f7b747d359f134553fdf93bae
Martastain <martas@imm.cz>
2016-04-06 17:52:03 +0200

    added settings to template

--- nebula/__init__.py
+++ nebula/__init__.py
@@ -5,12 +5,13 @@ from .helpers import *
 
 from .template_meta import meta_types
 from .template_folders import folders
+from .template_settings import settings
 
 template_data = {
         "meta_types" : meta_types,
         "folders" : folders,
         "cs" : [],
-        "settings" : [],
+        "settings" : settings,
         "actions" : {},
         "channels" : {},
         "services" : {},
