deebde7878d0e77736483389f1467c14ba62f6a2
Martastain <martas@imm.cz>
2016-12-18 11:08:09 +0100

    views templates

---
+++ templates/views.py
@@ -0,0 +1,18 @@
+VIEWS = {
+
+    1: {
+        "title" : "movies",
+        "owner" : 0,
+        "position" : 1,
+        "settings" : """<view>
+    <columns>
+        <column>title</column>
+        <column>role/director</column>
+        <column>year</column>
+        <column>duration</column>
+    </columns>
+</view>"""
+    }
+
+
+}
