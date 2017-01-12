2db837fb02da6d25533dd50b449997f562f7d64f
Martastain <martas@imm.cz>
2017-01-12 10:11:37 +0100

    view configuration

--- templates/views.py
+++ templates/views.py
@@ -3,8 +3,12 @@ VIEWS = {
         "title" : "Main",
         "owner" : 0,
         "position" : 1,
-        "settings" : """<view>
+        "settings" : """<?xml version="1.0"?>
+<view>
+    <folders>1,2</folders>
+    <statuses>0,1,2,5</statuses>
     <columns>
+        <column>promoted</column>
         <column>title</column>
         <column>role/director</column>
         <column>year</column>
@@ -14,20 +18,125 @@ VIEWS = {
     },
 
 
-2: {
+    2 : {
         "title" : "Fill",
         "owner" : 0,
         "position" : 2,
-        "settings" : """<view>
+        "settings" : """
+<view>
+    <folders>5,6,7,8</folders>
+    <statuses>0,1,2,5</statuses>
     <columns>
         <column>title</column>
+        <column>id_folder</column>
         <column>duration</column>
     </columns>
 </view>"""
     },
 
 
+    3 : {
+        "title" : "Music",
+        "owner" : 0,
+        "position" : 3,
+        "settings" : """<?xml version="1.0"?>
+<view>
+    <folders>4</folders>
+    <statuses>0,1,2,5</statuses>
+    <columns>
+        <column>promoted</column>
+        <column>title</column>
+        <column>role/performer</column>
+        <column>genre</column>
+        <column>duration</column>
+    </columns>
+</view>"""
+    },
 
 
+    4 : {
+        "title" : "Stories",
+        "owner" : 0,
+        "position" : 4,
+        "settings" : """<?xml version="1.0"?>
+<view>
+    <folders>3</folders>
+    <statuses>0,1,2,5</statuses>
+    <columns>
+        <column>title</column>
+        <column>genre</column>
+    </columns>
+</view>"""
+    },
 
-}
+
+    5 : {
+        "title" : "Commercial",
+        "owner" : 0,
+        "position" : 5,
+        "settings" : """<?xml version="1.0"?>
+<view>
+    <folders>9,10</folders>
+    <statuses>0,1,2,5</statuses>
+    <columns>
+        <column>title</column>
+        <column>genre</column>
+    </columns>
+</view>"""
+    },
+
+
+#
+# Auxiliary views
+#
+
+
+    10 : {
+        "title" : "-",
+        "owner" : 0,
+        "position" : 10,
+        "settings" : "<?xml version=\"1.0\"?><view/>"
+    },
+
+    11 : {
+        "title" : "Trash",
+        "owner" : 0,
+        "position" : 11,
+        "settings" : """<?xml version="1.0"?>
+<view>
+    <statuses>3</statuses>
+    <columns>
+        <column>title</column>
+    </columns>
+</view>"""
+    },
+
+    12 : {
+        "title" : "Archive",
+        "owner" : 0,
+        "position" : 12,
+        "settings" : """<?xml version="1.0"?>
+<view>
+    <statuses>4</statuses>
+    <columns>
+        <column>title</column>
+    </columns>
+</view>"""
+    },
+
+    13 : {
+        "title" : "Playout",
+        "owner" : 0,
+        "position" : 13,
+        "settings" : """<?xml version="1.0"?>
+<view>
+    <folders>13</folders>
+    <columns>
+        <column>title</column>
+        <column>id_storages</column>
+    </columns>
+</view>"""
+    },
+
+
+}  # END FOLDERS
