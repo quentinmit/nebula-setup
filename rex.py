bbb787ed087af785b641052c2a7801aeb6ea5240
Martastain <martas@imm.cz>
2017-08-11 16:53:58 +0200

    updated rex.py

--- rex.py
+++ rex.py
@@ -1,9 +1,13 @@
+__all__ = ["require"]
+
 import os
 import sys
 import subprocess
 import time
 import json
 import traceback
+
+###
 
 version_info = sys.version_info[:2]
 PYTHON_VERSION = version_info[0] + float("." + str(version_info[1])) # TODO: make this nice
@@ -20,9 +24,6 @@ else:
     decode_if_py3 = lambda x: x
     encode_if_py3 = lambda x: x
     string_type = unicode
-
-# do not import anything
-__all__ = []
 
 DEBUG, INFO, WARNING, ERROR, GOOD_NEWS = range(5)
 PLATFORM = "windows" if sys.platform == "win32" else "unix"
@@ -103,7 +104,7 @@ def critical_error(msg, **kwargs):
 
 
 
-class Repository():
+class Repository(object):
     def __init__(self, parent,  url, **kwargs):
         self.parent = parent
         self.url = url
@@ -169,7 +170,6 @@ class Rex(object):
         old_rex = open("rex.py").read()
         if new_rex != old_rex:
             logging.info("Updating REX core")
-            sys.exit(0)
             with open("rex.py", "w") as f:
                 f.write(new_rex)
         else:
@@ -212,7 +212,12 @@ class Rex(object):
         return True
 
     def post_install(self, repo):
-        if repo.get("python-path") and not repo.path in sys.path:
+        if (repo.get("python-path") or repo.get("python_path")) and not repo.path in sys.path:
             sys.path.insert(0, repo.path)
 
 rex = Rex()
+
+def require(url, **kwargs):
+    repo = Repository(rex, url, **kwargs)
+    return rex.update(repo) and rex.post_install(repo)
+
