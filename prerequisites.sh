746a372ef494b8bd08ecda68463524adaec231e4
Martastain <martas@imm.cz>
2017-08-17 15:50:34 +0200

    updatet prerequisites install script

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,12 +1,79 @@
 #!/bin/bash
 
-apt -y install \
-    python \
-    python-psycopg2 \
-    python-pylibmc \
-    python-psutil \
-    python-cairo \
-    python-imaging \
-    python-gtk2
+function install_db_server {
+    echo "Installing DB server"
+    apt -y install postgresql
+}
 
-pip install cherrypy
+function install_memcached {
+    echo "Installing memcached server"
+    apt -y install memcached
+}
+
+
+function install_node {
+    echo "Installing node server requirements"
+    apt install -y libmemcached-dev python3-pip cifs-utils
+    pip3 install pylibmc psutil psycopg2 pyyaml requests
+}
+
+function install_core {
+    echo "Installing core server requirements"
+    pip3 install cherrypy jinja2
+}
+
+
+do_install_db=0
+do_install_mc=0
+do_install_node=0
+do_install_core=0
+
+read -p "Install DB server? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    do_install_db=1
+fi
+
+read -p "Install memcached server? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    do_install_mc=1
+fi
+
+read -p "Install node? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    do_install_node=1
+
+    read -p "Install core server requirements? " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        do_install_core=1
+    fi
+fi
+
+
+
+
+echo ""
+
+if [ $do_install_db == 1 ]; then
+    install_db_server || exit 1
+fi
+
+if [ $do_install_mc == 1 ]; then
+    install_memcached || exit 1
+fi
+
+if [ $do_install_node == 1 ]; then
+    install_node || exit 1
+fi
+
+if [ $do_install_core == 1 ]; then
+    install_core || exit 1
+fi
+
